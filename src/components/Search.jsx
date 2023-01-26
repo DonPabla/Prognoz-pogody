6db8ac3353d73ec088484722fd92c800fd76fe47
DonPabla <113447403+DonPabla@users.noreply.github.com>
2023-01-26 23:21:04 +0600

    Add files via upload

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import module from './Search.module.scss'
 
 function Search(props) {
+    function myFunction() {
+
+    }
     return (
         <div>
-            
+            <input type="text" onChange="myFunction()" />
         </div>
+
     );
 }
 
