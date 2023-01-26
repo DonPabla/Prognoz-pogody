90e503380ab714019e18ce843c715ae4de8206c3
DonPabla <113447403+DonPabla@users.noreply.github.com>
2023-01-26 22:32:19 +0600

    Add files via upload

---
+++ src/components/WeatherCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import module from './WeatherCard.module.scss'
+import cloud from '../assets/cloudspng.png'
+
+function WeatherCard(props) {
+    return (
+        <div className={module.container}>
+            <div className={module.card}>
+                <h2>Алматы</h2>
+                <div className={module.card_image}>
+                    <img src={cloud} />
+                </div>
+                <div className={module.temperature}>
+                    +4°
+                </div>
+                <div className={module.currentDay}>
+                    {/* {currentDay} */}
+                </div>
+                <div className={module.currentDate}>
+                    {/* {currentDate} */}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default WeatherCard;
